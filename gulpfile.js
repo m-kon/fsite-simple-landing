49816df0941528b34c2a636663c232cd4b7bceaa
m-kon <mark.konstantinovi4@gmail.com>
2018-07-15 05:51:21 +0300

    install gulp4, fix syntax in gulpfile.js, remove  css

--- gulpfile.js
+++ gulpfile.js
@@ -6,23 +6,25 @@ const autoprefixer = require('autoprefixer');
 const exec = require('child_process').exec;
 
 gulp.task('sass', function() {
-    gulp.src('sass/*.sass')
+    return gulp.src('sass/*.sass')
         .pipe(sass())
         .pipe(gulp.dest('css/'));
 });
 
 gulp.task('autoprefixer', function () {
-    gulp.src('css/*.css')
+    return gulp.src('css/*.css')
         .pipe(sourcemaps.init())
         .pipe(postcss([ autoprefixer() ]))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./css'));
 });
 
-gulp.task('watch', ['sass', 'autoprefixer'], function() {
-    gulp.watch('sass/*.sass', ['sass']);
-});
+gulp.task('watch', gulp.series('sass', 'autoprefixer', function(done) {
+    gulp.watch('sass/*.sass', gulp.series('sass'));
+    done();
+}));
 
-gulp.task('default', ['watch'], function() {
+gulp.task('default', gulp.series('watch', function(done) {
   exec('live-server');
-});
+  done();
+}));
